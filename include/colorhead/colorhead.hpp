098315be7d7f2515dd1499daa9b6074dbd5b4e44
savethebees <afghanistaneurobolivar@gmail.com>
2026-06-24 20:55:13 +0300

    add rgb color funcs

--- include/colorhead/colorhead.hpp
+++ include/colorhead/colorhead.hpp
@@ -15,4 +15,14 @@ std::string bg_console_builtin(std::string str, int clr_num)
     return "\033[48;5;" + std::to_string(clr_num) + "m" + std::move(str) + "\033[0m";
 }
 
+std::string text_console_rgb(std::string str, int r, int g, int b)
+{
+    return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
+}
+
+std::string bg_console_rgb(std::string str, int r, int g, int b)
+{
+    return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
+}
+
 }
