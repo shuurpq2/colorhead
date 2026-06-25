05b7617a1c4ad0462e740cbd320c5c2ec57ca9eb
savethebees <afghanistaneurobolivar@gmail.com>
2026-06-25 16:08:27 +0300

    optimized lot reset commands

--- include/colorhead/colorhead.hpp
+++ include/colorhead/colorhead.hpp
@@ -6,32 +6,34 @@
 namespace clrhd
 {
 
-std::string color_text_old(std::string str, int clr_num) // color text in console built-in color (0-255). Sense - weights 9-11 bytes, when rgb weights 14-17 bytes
+std::string add_reset(std::string str);
+
+inline std::string color_text_old(std::string str, int clr_num) // color text in console built-in color (0-255). Sense - weights 9-11 bytes, when rgb weights 14-17 bytes
 {
-    return "\033[38;5;" + std::to_string(clr_num) + "m" + std::move(str) + "\033[0m";
+    return add_reset("\033[38;5;" + std::to_string(clr_num) + "m" + std::move(str));
 }
 
-std::string color_bg_old(std::string str, int clr_num)
+inline std::string color_bg_old(std::string str, int clr_num)
 {
-    return "\033[48;5;" + std::to_string(clr_num) + "m" + std::move(str) + "\033[0m";
+    return add_reset("\033[48;5;" + std::to_string(clr_num) + "m" + std::move(str));
 }
 
-std::string color_text_rgb(std::string str, int r, int g, int b)
+inline std::string color_text_rgb(std::string str, int r, int g, int b)
 {
-    return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
+    return add_reset("\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str));
 }
 
-std::string color_bg_rgb(std::string str, int r, int g, int b)
+inline std::string color_bg_rgb(std::string str, int r, int g, int b)
 {
-    return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
+    return add_reset("\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str));
 }
 
-std::string decorate_text(std::string str)
+inline std::string decorate_text(std::string str)
 {
-    return std::move(str) + "\033[0m";
+    return add_reset(std::move(str));
 }
 template<typename... Args>
-std::string decorate_text(std::string str, TextDecoration decoration, Args&&... decorations)
+inline std::string decorate_text(std::string str, TextDecoration decoration, Args&&... decorations)
 {
     return decorate_text("\033[" + std::to_string(static_cast<int>(decoration)) + "m" + std::move(str), decorations...);
 }
--- src/colorhead.cpp
+++ src/colorhead.cpp
@@ -1,1 +1,13 @@
 #include "colorhead/colorhead.hpp"
+
+namespace clrhd {
+
+std::string add_reset(std::string str) {
+    if (std::string_view(str.data() + str.size()-4) != "\033[0m") {
+        str += "\033[0m";
+    }
+
+    return std::move(str);
+}
+
+}
