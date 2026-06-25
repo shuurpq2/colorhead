2f1c190497999037f72e7750efdb3c277bd5097a
savethebees <afghanistaneurobolivar@gmail.com>
2026-06-25 15:39:53 +0300

    add text decorations

--- include/colorhead/colorhead.hpp
+++ include/colorhead/colorhead.hpp
@@ -1,28 +1,39 @@
 #pragma once
 
+#include "colorhead/text_decoration.hpp"
 #include <string>
 
 namespace clrhd
 {
 
-std::string text_console_builtin(std::string str, int clr_num) // color text in console built-in color (0-255). Sense - weights 9-11 bytes, when rgb weights 14-17 bytes
+std::string color_text_old(std::string str, int clr_num) // color text in console built-in color (0-255). Sense - weights 9-11 bytes, when rgb weights 14-17 bytes
 {
     return "\033[38;5;" + std::to_string(clr_num) + "m" + std::move(str) + "\033[0m";
 }
 
-std::string bg_console_builtin(std::string str, int clr_num)
+std::string color_bg_old(std::string str, int clr_num)
 {
     return "\033[48;5;" + std::to_string(clr_num) + "m" + std::move(str) + "\033[0m";
 }
 
-std::string text_console_rgb(std::string str, int r, int g, int b)
+std::string color_text_rgb(std::string str, int r, int g, int b)
 {
     return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
 }
 
-std::string bg_console_rgb(std::string str, int r, int g, int b)
+std::string color_bg_rgb(std::string str, int r, int g, int b)
 {
     return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + std::move(str) + "\033[0m";
+}
+
+std::string decorate_text(std::string str)
+{
+    return str;
+}
+template<typename... Args>
+std::string decorate_text(std::string str, TextDecoration decoration, Args&&... decorations)
+{
+    return decorate_text("\033[" + std::to_string(static_cast<int>(decoration)) + "m" + std::move(str) + "\033[0m", decorations...);
 }
 
 }
